193093bef15d400c7520577a4ee20034635a7a95
SashaSNky <sanek-avto@yandex.by>
2023-01-15 18:32:26 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let min=0;
+       if (n==0) return 0;
+       else {
+       if  (n<0) min=1;}   
+       let res='';
+       let str=String(n);
+         for (let i=str.length-1; i>=min; i--) {
+         res +=str[i];
+        }
+        return Number(res);
 }
+
+
+
